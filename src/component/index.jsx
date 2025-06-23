05076d21ad84b7ba2500478e67e93eb11f3340a8
seu nome de usuário <pealq63@gmail.com>
2025-06-23 17:03:04 -0300

    adicionado banner no top da pagina

---
+++ src/component/index.jsx
@@ -0,0 +1,13 @@
+import "./banner.css"
+
+const banner = () => {
+    return (
+        <div className="banner">
+            <img src="/imagem/banner-lol-lane.webp" className="img" alt="banner das lane do Lol"></img>
+        </div>
+
+    )
+        
+}
+
+export default banner
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Banner from './component/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Banner/>
   </StrictMode>,
 )
