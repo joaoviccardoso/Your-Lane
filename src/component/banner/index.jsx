a17dfd6b775af5e454a578b62e24ffb0fffdc4d2
seu nome de usuário <pealq63@gmail.com>
2025-06-30 09:34:35 -0300

    melhorado o estilo da pagina

--- src/component/banner/index.jsx
+++ src/component/banner/index.jsx
@@ -4,6 +4,10 @@ const banner = () => {
     return (
         <div className="banner">
             <img src="../imagem/464908915_8649501041832613_3778883029003467533_n.png" className="img" alt="banner das lane do Lol"></img>
+            <div>
+                <h1>Bem Vindo!</h1>
+                <p>Qual sua lane favorito de jogar gostaria muito de saber.</p>
+            </div>
         </div>
 
     )
